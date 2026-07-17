a0fbf76d22e0f08962c64f8d063340fd37ff59dc
Bharathi-vollmoon <bharathi.a@vollmoon.com>
2026-07-17 12:31:09 +0530

    another way of thread example  using Runnable

--- src/JavaThread.java
+++ src/JavaThread.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.util.*;
 
 //extending thread method
-
+/*
 class myThread extends  Thread{
     public void run(){
-        String str = "thread is running good";
+        String str = "thread is running by using extends thread";
         System.out.println(str);
     }
 }
@@ -14,5 +14,20 @@ public class JavaThread {
     public static void main(String args[]){
             myThread call = new myThread();
             call.start();
+
+            */
+class myThread implements Runnable {
+    public void run() {
+        String str = "thread is running by using Runnable implemments ";
+        System.out.println(str);
     }
 }
+    public class JavaThread {
+        public static void main(String args[]) {
+            myThread call = new myThread();
+
+            Thread fam = new Thread(call);
+            fam.start();
+        }
+    }
+
