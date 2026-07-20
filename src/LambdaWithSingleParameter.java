4dddc95756bc57482cdde0f433fb80b4d2f3f061
Bharathi-vollmoon <bharathi.a@vollmoon.com>
2026-07-20 18:13:29 +0530

    add corection

--- src/LambdaWithSingleParameter.java
+++ src/LambdaWithSingleParameter.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class LambdaWithSingleParameter {
+    public void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println("all elements");
+
+        list.forEach(n -> System.out.println(n));
+
+        System.out.println("even elements ");
+        list.forEach(n ->
+
+        {
+          if (n%2 == 0){
+              System.out.println(n);
+
+          }
+        });
+
+    }
 }
